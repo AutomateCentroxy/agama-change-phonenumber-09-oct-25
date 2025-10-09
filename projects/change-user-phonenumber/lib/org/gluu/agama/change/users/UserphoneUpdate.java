e224f34d9c4558e4b40aebc78a8a094346fc87b1
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-10-09 13:19:29 +0530

    feat: add UserphoneUpdate.java

---
+++ projects/change-user-phonenumber/lib/org/gluu/agama/change/users/UserphoneUpdate.java
@@ -0,0 +1,24 @@
+package org.gluu.agama.change.users;
+
+import java.util.Map;
+import org.gluu.agama.change.smschange.PhonenumberUpdate;
+
+
+
+public abstract class UserphoneUpdate {
+    public abstract String addNewUser(Map<String, String> profile) throws Exception;
+
+    public abstract boolean usernamePolicyMatch(String userName);
+
+    public abstract boolean passwordPolicyMatch(String userPassword);
+
+    public abstract String updateUser(Map<String, String> profile) throws Exception;
+
+    public abstract Map<String, String> getUserEntityByInum(String inum);
+
+    public abstract boolean sendUsernameUpdateEmail(String to, String newUsername, String lang);
+
+    public static UsernameUpdate getInstance(HashMap config){
+        return  JansUsernameUpdate.getInstance(config);
+    }    
+}
